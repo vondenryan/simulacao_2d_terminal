e284f7a7b5babf0c4a2e9f78b48138662b7471d0
vondenryan <vonden.ryan@gmail.com>
2026-06-18 19:12:26 -0300

    refactor: MapController now handles all the map logic

--- src/com/simulacao_terminal/controllers/MapController.java
+++ src/com/simulacao_terminal/controllers/MapController.java
@@ -1,88 +1,85 @@
 package com.simulacao_terminal.controllers;
 
-import java.util.Random;
-import java.util.Scanner;
-
 import com.simulacao_terminal.utils.PerlinNoise;
+import java.util.Random;
 
 public class MapController {
-    static Scanner in = new Scanner(System.in);
+    private int map[][];
+    private int mapWidth;
+    private int mapHeight;
+    private String terrainType;
+
     static Random random = new Random();
 
-    public int[][] generateMap(int y, int x, int generationType) {
-        int map[][] = new int[y][x];
+    public MapController(int mapHeight, int mapWidth, String terrainType) {
+        this.map = new int[mapHeight][mapWidth];
+        this.terrainType = terrainType;
+        this.mapHeight = mapHeight;
+        this.mapWidth = mapWidth;
 
-        switch (generationType) {
-            case 1:
-                map = flatMap(map);
+        switch (terrainType) {
+            case "flat":
+                flatMap();
                 break;
-            case 2:
-                map = naturalGeneration(map);
+            case "natural":
+                naturalGeneration();
                 break;
             default:
-                System.out.println("EROR! Invalid option!");
-                return new int[0][0];
+                //Assign flat as default
+                flatMap();
         }
-
-        return map;
     }
 
-    //! Generation Types
-    private int[][] flatMap(int m[][]) {
-        for(int lin = 0; lin < m.length; lin++) {
-            for(int col = 0; col < m[lin].length; col++) {
-                if(lin > m.length / 2) {
+    private void flatMap() {
+        for(int lin = 0; lin < map.length; lin++) {
+            for(int col = 0; col < map[lin].length; col++) {
+                if(lin > map.length / 2) {
                     if(Math.random() > 0.5) {
-                        m[lin][col] = 1;
+                        map[lin][col] = 1;
                     } else {
-                        m[lin][col] = 2;
+                        map[lin][col] = 2;
                     }
                 }
             }
         }
-
-        return m;
     }
-
-    private int[][] naturalGeneration(int m[][]) {
+    
+    private void naturalGeneration() {
         float surfaceScale = 0.02f;
-        float caveScale = 0.05f; // Controls cave size/frequency
+        float caveScale = 0.025f; // Controls cave size/frequency
         
         double offsetX = Math.random() * 300;
         double offsetY = Math.random() * 300;
     
-        for (int y = 0; y < m.length; y++) {
-            for (int x = 0; x < m[y].length; x++) {
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
                 
                 double surfaceNoise = PerlinNoise.noise((float)(offsetX + (x * surfaceScale)), 0.6f);
-                int groundLevel = m.length / 2 + (int)(surfaceNoise * 8);
-    
+                int groundLevel = map.length / 2 + (int)(surfaceNoise * 8);
+
                 if (y > groundLevel) {
                     float cX = (float)(offsetX + (x * caveScale));
                     float cY = (float)(offsetY + (y * caveScale * 2.0f));
                     float caveNoise = (PerlinNoise.noise(cX, cY) + 1.0f) / 2.0f;
     
-                    if (y > groundLevel + 2 && caveNoise > 0.68f) {
-                        m[y][x] = 0; // Cave air
+                    if (y > groundLevel + 15 && caveNoise > 0.68f) {
+                        map[y][x] = 0; // Cave air
                     } 
                     else {
                         if (y == groundLevel + 1) {
-                            m[y][x] = 1; // Grass surface
+                            map[y][x] = 1; // Grass surface
                         } else if (y <= groundLevel + 5) {
-                            m[y][x] = 2; // Shallow Dirt layer
+                            map[y][x] = 2; // Shallow Dirt layer
                         } else {
-                            m[y][x] = 4; // Deep Stone layer
+                            map[y][x] = 4; // Deep Stone layer
                         }
                     }
                 } else {
-                    m[y][x] = 0; // Sky Air
+                    map[y][x] = 0; // Sky Air
                 }
             }
         }
-        return m;
     }
-
-    //! Player WorldPos Controller Methods
 
     public int[] spawnPlayer(int m[][]) {
         int minX = 1;
@@ -103,5 +100,37 @@ public class MapController {
         }
     
         return new int[] {y, x};
+    }
+
+    public int[][] getMap() {
+        return map;
+    }
+
+    public void setMap(int[][] map) {
+        this.map = map;
+    }
+
+    public String getTerrainType() {
+        return terrainType;
+    }
+
+    public void setTerrainType(String terrainType) {
+        this.terrainType = terrainType;
+    }
+
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public void setMapWidth(int mapWidth) {
+        this.mapWidth = mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
+
+    public void setMapHeight(int mapHeight) {
+        this.mapHeight = mapHeight;
     }
 }
